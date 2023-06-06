8b6fb90d2c8288e4004fecd5d07ebec03e814c2c
MatiasCra <cravchikm@gmail.com>
2023-06-06 10:56:56 -0300

    ej2 y readme

---
+++ ej2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Ej 2" << endl;
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# TP 3 Algo3 1er cuatrimestre 2023
+## Por Tomás Jarabroviski y Matías Cravchik
+
+### TODO
