8a173ac91e6143c974fb5de5229da7874ec85030
MatiasCra <cravchikm@gmail.com>
2023-06-14 13:26:34 -0300

    ej2

--- ej2/ej2.cpp
+++ ej2/ej2.cpp
@@ -54,8 +54,65 @@ int maxflow(int s, int t) {
     return flow;
 }
 
+enum Preferencia { PRIM = 0, KRUSKAL = 1 };
+
+void leer_input() {
+    capacity = vector<vector<int>>(n, vector<int>(n, 0));
+    adj = vector<vector<int>>(n);
+    int cant_relaciones;
+    cin >> cant_relaciones;
+
+    vector<int> preferencias(n - 2, PRIM);
+    for (int i = 0; i < n - 2; ++i) {
+        int aux;
+        cin >> aux;
+        preferencias[i] = aux;
+    }
+
+    int s = n - 2;
+    int t = n - 1;
+    for (int i = 0; i < n - 2; ++i) {
+        if (preferencias[i] == KRUSKAL) {
+            adj[s].emplace_back(i);
+            adj[i].emplace_back(s);
+            capacity[s][i] = 1;
+        } else {
+            adj[i].emplace_back(t);
+            adj[t].emplace_back(i);
+            capacity[i][t] = 1;
+        }
+    }
+
+    for (int i = 0; i < cant_relaciones; ++i) {
+        int v, u;
+        cin >> v >> u;
+        --v;
+        --u;
+        adj[v].emplace_back(u);
+        adj[u].emplace_back(v);
+        if (preferencias[v] == PRIM and preferencias[u] == KRUSKAL) swap(v, u);
+        capacity[v][u] = 1;
+
+        if (preferencias[v] == preferencias[u]) capacity[u][v] = 1;
+    }
+}
+
 int main() {
-    // TODO
+    // IO rápido con cin/cout
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (cin >> n) {
+        if (n == 0) break;
+
+        n += 2;
+        int s = n - 2;
+        int t = n - 1;
+        leer_input();
+
+        int flujo = maxflow(s, t);
+        cout << flujo << "\n";
+    }
 
     return 0;
 }
